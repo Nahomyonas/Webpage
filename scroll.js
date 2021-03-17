5c61ba42ab07c54dc1bde74e5a87d6cac341fc3f
Larcade-1909 <your_email@example.com>
2021-03-17 11:54:40 +0300

    edited' sto stop clear ' clr git commit -m edited sto stop clear

--- scroll.js
+++ scroll.js
@@ -15,5 +15,22 @@ observer = new IntersectionObserver((entries) => {
 sections.forEach(section => {
     observer.observe(section)
 })
+const animations = document.querySelectorAll('.animate1');
+
+observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.style.animation = `anim2 2s ${entry.target.dataset.delay} forwards ease-out`;
+        } else {
+            entry.target.style.animation = 'none';
+        }
+    })
+
+})
+
+animations.forEach(animation => {
+    observer.observe(animation)
+})
 
 
